12b233c46535c94480b6b459ec69f21ea411bf7a
prik73 <prinovac@gmail.com>
2025-05-13 14:21:08 +0530

    changes in server script

--- prik/src/components/Footer.jsx
+++ prik/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 
 export default function Footer({ count }) {
   return (
-    <div className="text-center text-sm text-gray-500 py-4 ml-450">
+    <div className="text-center text-sm text-gray-500 py-4 ml-350">
       times visited{' '}
       <span className="font-bold text-indigo-600">{count ?? '...'}</span> times.
     </div>
